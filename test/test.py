27e76d2c8dbb263c066d4fbb60fdb17c333e61dd
OnSachinSharma <145487989+OnSachinSharma@users.noreply.github.com>
2025-02-19 13:08:48 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -16,12 +16,12 @@ async def test_project(dut):
 
     # Reset
     dut._log.info("Reset asserted")
-    dut.ena.value = 1
+    dut.ena.value = 0
     dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 1
     await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 0
+    dut.rst_n.value = 1
     dut._log.info(f"Reset deasserted, rst_n={dut.rst_n.value}")
     
 
