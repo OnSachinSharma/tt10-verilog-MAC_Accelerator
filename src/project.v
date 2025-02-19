5367084b3e28ec800357eb40aaaa8dbc5774ef73
OnSachinSharma <145487989+OnSachinSharma@users.noreply.github.com>
2025-02-19 13:22:21 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -19,8 +19,8 @@ module tt_um_MAC_Accelerator_OnSachinSharma
 
 // All output pins must be assigned. If not used, assign to 0.
     
-    assign uio_out=0;
-    assign uio_oe=0;
+   // assign uio_out=0;
+      assign uio_oe=0;
     
     
     mac_vedicmul_adder DUT ( .a(ui_in[3:0]), .b(ui_in[7:4]), .C(uo_out), .X(uio_out[3:0]), .Y(uio_out[7:4]), .rst(rst_n), .clk(clk));
